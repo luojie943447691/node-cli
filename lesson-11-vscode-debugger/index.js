87daed9c0cdf6c42388b85d8c02742933cefa52f
lj1946235928 <943447691@qq.com>
2024-11-20 14:46:17 +0800

    feat: nodejs pnpm调试配置

---
+++ lesson-11-vscode-debugger/index.js
@@ -0,0 +1,17 @@
+// {
+//   "name": "Launch via PNPM",
+//   "request": "launch",
+//   "runtimeArgs": [
+//     "run-script",
+//     "debug"
+//   ],
+//   "runtimeExecutable": "pnpm",
+//   "console": "integratedTerminal",
+//   "skipFiles": [
+//     "<node_internals>/**"
+//   ],
+//   "type": "node",
+//   "cwd": "${workspaceFolder}/lesson-11-vscode-debugger"
+// }
+
+console.log(process.argv)
